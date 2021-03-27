3f3c1a96311c82e92aee82063224be819a014c22
KuroNeko98 <80913307+KuroNeko98@users.noreply.github.com>
2021-03-27 20:09:59 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=OTc6CRoZSxFmbltTNYPem8rghmGK6O4orLDIfwUt";
         
         $("#loadImage").click(function(){
             $.ajax({
